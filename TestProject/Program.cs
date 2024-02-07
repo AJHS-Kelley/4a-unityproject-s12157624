3804663c4f132e94907f2b22d73bae034ff52de3
Bruce J <90473269+s12157624@users.noreply.github.com>
2024-02-06 16:37:25 -1100

    :3

---
+++ TestProject/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TestProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int myInt = 33;
+        float myFloat = 8.263719F;
+        bool pineappleBestFruit = true;
+        string loveMe = "NOOOOOO";
+
+        Console.WriteLine("I have " + myInt + " kids");
+        Console.WriteLine("You look about " + myFloat + " years old");
+        Console.WriteLine("Pineapple is the besst fruit:" + pineappleBestFruit);
+        Console.WriteLine("Does the love me man" + loveMe);
+
+        for (int i = 0; i < 20; i++) {
+            Console.WriteLine("I ate " + i + " slices of pizza for lunch.\n");
+        }
+    }
+}
